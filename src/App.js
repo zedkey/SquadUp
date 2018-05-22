d69cee5fccd5d0bb1fda32b749920403aebc3f72
Zach Kolodny <zach@Zachs-MacBook-Pro.local>
2018-05-22 19:03:05 -0400

    added minimal homepage and started login

---
+++ src/App.js
@@ -0,0 +1,62 @@
+import React, { Component, Fragment } from 'react';
+import { Link } from 'react-router-dom';
+import { LinkContainer } from "react-router-bootstrap";
+import { Nav, Navbar, NavItem } from 'react-bootstrap';
+import "./App.css";
+import Routes from "./Routes";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isAuthenticated : false
+    }
+  }
+
+  userHasAuthenticated = (authenticated) => {
+    this.setState({isAuthenticated : true});
+  }
+
+  handleLogout = (event) => {
+    this.setState({isAuthenticated : false });
+  }
+
+  render() {
+    const childProps = {
+      isAuthenticated : this.state.isAuthenticated,
+      userHasAuthenticated : this.userHasAuthenticated
+    };
+
+    return (
+      <div className="App container">
+        <Navbar fluid collapseOnSelect>
+          <Navbar.Header>
+            <Navbar.Brand>
+              <Link to="/">All</Link>
+            </Navbar.Brand>
+            <Navbar.Toggle />
+          </Navbar.Header>
+          <Navbar.Collapse>
+            <Nav pullRight>
+              {this.state.isAuthenticated ?
+                <NavItem onClick={this.handleLogout}>Logout</NavItem> :
+                <Fragment>
+                  <LinkContainer to="/register">
+                    <NavItem>Register</NavItem>
+                  </LinkContainer>
+                  <LinkContainer to="/login">
+                    <NavItem>Login</NavItem>
+                  </LinkContainer>
+                </Fragment>
+              }
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+        <Routes childProps={childProps} />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/containers/Home/Home.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import "./Home.css";
+
+export default class Home extends Component {
+  render() {
+    return (
+      <div className="Home">
+        <div className="lander">
+          <h1>LookingFor</h1>
+          <p>An app designed to connect people/friends for even the most basic of requests</p>
+        </div>
+      </div>
+    );
+  }
+}
