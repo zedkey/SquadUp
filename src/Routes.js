7da2dc32a750db25d7742d49b0d20d5aae6f066f
Zach Kolodny <zach@Zachs-MBP.home>
2018-05-31 14:45:14 -0400

    updated routes

--- src/Routes.js
+++ src/Routes.js
@@ -1,13 +1,13 @@
 import React from "react";
-import { Route, Switch } from "react-router-dom";
+import {Route, Switch} from "react-router-dom";
 import Home from "./containers/Home/Home";
 import NotFound from "./containers/NotFound/NotFound";
 import Login from "./containers/Login/Login";
 import AppliedRoute from "./containers/AppliedRoute/AppliedRoute";
 
-export default ({ childProps }) =>
-  <Switch>
-    <AppliedRoute path="/" exact component={Home} props={childProps} />
-    <AppliedRoute path="/login" exact component={Login} props={childProps} />
-    <Route component={NotFound} />
-  </Switch>;
+export default({childProps}) =>
+<Switch>
+  <AppliedRoute path="/" exact="exact" component={Home} props={childProps}/>
+  <AppliedRoute path="/login" exact="exact" component={Login} props={childProps}/>
+  <Route component={NotFound}/>
+</Switch>;
