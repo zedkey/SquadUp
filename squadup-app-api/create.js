a9c50e25728632313e834966838878bd647bd01f
zkolodny <zbkolodny@me.com>
2018-06-01 12:56:16 -0400

    fixed paths to lib files

--- squadup-app-api/create.js
+++ squadup-app-api/create.js
@@ -1,6 +1,6 @@
 import uuid from "uuid";
-import * as dynamoDbLib from "./libs/dynamodb-lib";
-import { success, failure } from "./libs/response-lib";
+import * as dynamoDbLib from "../libs/dynamodb-lib";
+import { success, failure } from "../libs/response-lib";
 
 AWS.config.update({region:"us-east-2"});
 const dynamoDB = new AWS.DynamoDB.DocumentClient();
